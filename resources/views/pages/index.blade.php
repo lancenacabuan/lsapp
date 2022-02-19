a69b4f628ddadbf50a64eb94a2e63bb2415415d7
c4lance <lancenacabuan@outlook.com>
2022-02-19 17:10:39 +0800

    edit dashboard

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -2,57 +2,53 @@
 @section('content')
 <script src="{{ asset('js/home.js') }}"></script>
     <div class="container pt-3">
-    <div class="animate fadeInDown two">
-        <center>
         <div class="container-fluid">
-                <div class="row">
-                    <div class="col-sm-3">
-                        <a href="stocks" style="text-decoration:none">
-                            <div class="container" style="background-color: #0d1a80; color: white; border-radius: 100%; height: 150px; width: 150px; font-size: 30px; text-align: center; display: inline-flex; align-items: center; justify-content: center;">
-                            <center>{{DB::table('stocks')->count();}}</center>
-                            </div>
-                            <br>
-                            <strong style="color: #0d1a80; font-size: 20px;">STOCKS</strong>
-                        </a>
-                    </div>
-                    <div class="col-sm-3">
-                        <a href="stockrequest" style="text-decoration:none">
-                            <div class="container" style="background-color: #0d1a80; color: white; border-radius: 100%; height: 150px; width: 150px; font-size: 30px; text-align: center; display: inline-flex; align-items: center; justify-content: center;">
-                            <center>{{DB::table('requests')->whereNotIn('requests.status',['7','8'])->get()->count();}}</center>
-                            </div>
-                            <br>
-                            <strong style="color: #0d1a80; font-size: 20px;">STOCK REQUEST</strong>
-                        </a>
-                    </div>
-                    <div class="col-sm-3">
-                        <a href="stocktransfer" style="text-decoration:none">
-                            <div class="container" style="background-color: #0d1a80; color: white; border-radius: 100%; height: 150px; width: 150px; font-size: 30px; text-align: center; display: inline-flex; align-items: center; justify-content: center;">
-                            <center>{{DB::table('request_transfer')->whereNotIn('request_transfer.status',['7','8'])->get()->count();}}</center>
-                            </div>
-                            <br>
-                            <strong style="color: #0d1a80; font-size: 20px;">STOCK TRANSFER</strong>
-                        </a>
-                    </div>
+        <center>
+            <div class="row">
+                <div class="col-sm-4">
+                    <a href="stocks" style="text-decoration:none">
+                        <div class="container" style="background-color: #0d1a80; color: white; border-radius: 100%; height: 150px; width: 150px; font-size: 30px; text-align: center; display: inline-flex; align-items: center; justify-content: center;">
+                        <center>{{DB::table('stocks')->whereIn('stocks.location_id',['1','2','3','4','5','6'])->get()->count();}}</center>
+                        </div>
+                        <br>
+                        <strong style="color: #0d1a80; font-size: 20px;">STOCKS</strong>
+                    </a>
                 </div>
-        </div>
+                <div class="col-sm-4">
+                    <a href="stockrequest" style="text-decoration:none">
+                        <div class="container" style="background-color: #0d1a80; color: white; border-radius: 100%; height: 150px; width: 150px; font-size: 30px; text-align: center; display: inline-flex; align-items: center; justify-content: center;">
+                        <center>{{DB::table('requests')->whereNotIn('requests.status',['7','8'])->get()->count();}}</center>
+                        </div>
+                        <br>
+                        <strong style="color: #0d1a80; font-size: 20px;">STOCK REQUEST</strong>
+                    </a>
+                </div>
+                <div class="col-sm-4">
+                    <a href="stocktransfer" style="text-decoration:none">
+                        <div class="container" style="background-color: #0d1a80; color: white; border-radius: 100%; height: 150px; width: 150px; font-size: 30px; text-align: center; display: inline-flex; align-items: center; justify-content: center;">
+                        <center>{{DB::table('request_transfer')->whereNotIn('request_transfer.status',['7','8'])->get()->count();}}</center>
+                        </div>
+                        <br>
+                        <strong style="color: #0d1a80; font-size: 20px;">STOCK TRANSFER</strong>
+                    </a>
+                </div>
+            </div>
         </center>
-    </div>
+        </div>
     </div>
     <br>
     <div class="container-fluid">
         <div class="panel-body table-responsive "><br>
-            <div class="animate fadeInDown two">
-                <table id="user_logs" class="table user_logs display nowrap" style="width:100%;font-size:12px;">
-                    <thead style="background-color: #0d1a80 !important; color: white !important; font-weight: bold !important; font-size: 15px;">
-                        <tr>
-                            <th>DATE</th>
-                            <th>NAME</th>
-                            <th>USER LEVEL</th>
-                            <th>ACTIVITY</th>
-                        </tr>
-                    </thead>                 
-                </table>
-            </div>
+            <table id="user_logs" class="table user_logs display nowrap" style="width:100%;font-size:12px;">
+                <thead style="background-color: #0d1a80 !important; color: white !important; font-weight: bold !important; font-size: 15px;">
+                    <tr>
+                        <th>DATE</th>
+                        <th>NAME</th>
+                        <th>USER LEVEL</th>
+                        <th>ACTIVITY</th>
+                    </tr>
+                </thead>                 
+            </table>
         </div>
     </div>
 @endsection

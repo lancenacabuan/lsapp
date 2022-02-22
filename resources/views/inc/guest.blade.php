3e5a6dab79bac222207fc1a535ecb131c8ae1fe5
c4lance <lancenacabuan@outlook.com>
2022-02-22 10:17:38 +0800

    guest header

--- resources/views/inc/guest.blade.php
+++ resources/views/inc/guest.blade.php
@@ -1,5 +1,5 @@
 <a href="/" style="text-decoration: none;">
 <div><center><img class="p-2 align-self-end" src="/storage/idsi.png" alt="idsi.png" style="width: auto; height: 150px;"></center></div>
-<div style="color: white; font-family: arial; font-weight: bold !important; background-color: #0d1a80;"><h3 class="p-2 align-self-end"><center style="margin-top: 10px; margin-bottom: 10px;">MAIN WAREHOUSE STOCK MONITORING SYSTEM</center></h3></div>
+<div style="color: white; font-family: arial; background-color: #0d1a80;"><h3 class="p-2 align-self-end"><center style="margin-top: 15px; margin-bottom: 15px; font-weight: bold !important;">MAIN WAREHOUSE STOCK MONITORING SYSTEM</center></h3></div>
 </a>
 <br><br>

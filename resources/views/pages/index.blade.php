d4f42d6a519da68cba7ea3d26ceb13b86f4a3b63
c4lance <lancenacabuan@outlook.com>
2022-03-03 16:56:09 +0800

    dashboard icons

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -7,8 +7,9 @@
             <div class="row">
                 <div class="col-sm-4" style="margin-left: 100px; margin-right: -100px;">
                     <a href="stocks" style="text-decoration:none">
-                        <div class="container" style="background-color: #0d1a80; color: white; border-radius: 100%; height: 150px; width: 150px; font-size: 30px; text-align: center; display: inline-flex; align-items: center; justify-content: center;">
-                        <center>{{DB::table('stocks')->whereIn('stocks.location_id',['1','2','3','4','5','6'])->get()->count();}}</center>
+                        <div class="container" style="background-color: #0d1a80; color: white; border-radius: 100%; height: 150px; width: 150px; font-size: 30px; text-align: center; align-items: center; justify-content: center; vertical-align: center;">
+                            <br><center>{{DB::table('stocks')->whereIn('stocks.location_id',['1','2','3','4','5','6'])->get()->count();}}</center>
+                            <i class="fa fa-cart-arrow-down" style="font-size: 50px"></i>
                         </div>
                         <br>
                         <strong style="color: #0d1a80; font-size: 20px;">STOCKS</strong>
@@ -16,8 +17,9 @@
                 </div>
                 <div class="col-sm-4">
                     <a href="stockrequest" style="text-decoration:none">
-                        <div class="container" style="background-color: #0d1a80; color: white; border-radius: 100%; height: 150px; width: 150px; font-size: 30px; text-align: center; display: inline-flex; align-items: center; justify-content: center;">
-                        <center>{{DB::table('requests')->whereNotIn('requests.status',['7','8'])->get()->count();}}</center>
+                        <div class="container" style="background-color: #0d1a80; color: white; border-radius: 100%; height: 150px; width: 150px; font-size: 30px; text-align: center; align-items: center; justify-content: center;">
+                            <br><center>{{DB::table('requests')->whereNotIn('requests.status',['7','8'])->get()->count();}}</center>
+                            <i class="fa fa-truck" style="font-size: 50px"></i>
                         </div>
                         <br>
                         <strong style="color: #0d1a80; font-size: 20px;">STOCK REQUEST</strong>
@@ -25,8 +27,9 @@
                 </div>
                 <div class="col-sm-4" style="margin-left: -100px;">
                     <a href="stocktransfer" style="text-decoration:none">
-                        <div class="container" style="background-color: #0d1a80; color: white; border-radius: 100%; height: 150px; width: 150px; font-size: 30px; text-align: center; display: inline-flex; align-items: center; justify-content: center;">
-                        <center>{{DB::table('request_transfer')->whereNotIn('request_transfer.status',['7','8'])->get()->count();}}</center>
+                        <div class="container" style="background-color: #0d1a80; color: white; border-radius: 100%; height: 150px; width: 150px; font-size: 30px; text-align: center; align-items: center; justify-content: center;">
+                            <br><center>{{DB::table('request_transfer')->whereNotIn('request_transfer.status',['7','8'])->get()->count();}}</center>
+                            <i class="fa fa-exchange" style="font-size: 50px"></i>
                         </div>
                         <br>
                         <strong style="color: #0d1a80; font-size: 20px;">STOCK TRANSFER</strong>

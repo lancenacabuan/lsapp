dbfbbfa4dd1905a067161464a38d8627e7b70060
c4lance <lancenacabuan@outlook.com>
2022-03-09 15:02:52 +0800

    navbar header

--- resources/views/inc/header.blade.php
+++ resources/views/inc/header.blade.php
@@ -1,20 +1,18 @@
 <div class="d-flex">
     <a href="/">
-    <img class="p-2 align-self-end" src="/storage/idsi.png" style="width: auto; height: 90px; border-right:1px solid #3333">
+        <img class="p-2 align-self-end" src="/storage/idsi.png" style="width: auto; height: 95px; line-height: 95px; border-right:1px solid rgba(0, 0, 0, 0.2)">
     </a>
-    <a href="/" class="p-2 align-self-end" style="color: #0d1a80 !important; font-family: Arial !important; font-size:25px !important; margin-bottom:20px !important; text-decoration: none !important;">
-    <h3 style="font-weight: bold !important;">MAIN WAREHOUSE STOCK MONITORING SYSTEM</h3>
+    <a href="/" style="color: #0d1a80; font-family: Arial; font-weight: bold; font-size: 25px; line-height: 95px; margin-left: 10px; text-decoration: none;">
+        MAIN WAREHOUSE STOCK MONITORING SYSTEM
     </a>
-    <div class="p-2 ml-auto align-self-end d-flex">
-        <div class="p-2 ml-auto" style="text-align: right; font-size:12px;">
+    <div class="p-2 ml-auto align-self-end d-flex" style="margin-bottom: 10px;">
+        <div class="p-2 ml-auto" style="text-align: right; font-size: 12px;">
             {{Carbon\Carbon::now()->isoformat('dddd, MMMM D, YYYY')}}
-            <span id="datetime"></span></br>
+            <span id="datetime"></span><br>
             <strong>{{auth()->user()->name}}</strong>&nbsp;
-            [{{strtoupper(str_replace('"]','',(str_replace('["','',auth()->user()->getRoleNames()))))}}]</br>
-            {{auth()->user()->email}}</br>
-            <a style="color: black !important;" href="{{ url('/changepassword') }}">
-                Change Password
-            </a>
+            [{{strtoupper(str_replace('"]','',(str_replace('["','',auth()->user()->getRoleNames()))))}}]<br>
+            {{auth()->user()->email}}<br>
+            <a style="color: black;" href="{{ url('/changepassword') }}">Change Password</a>
         </div>
         <i class="fa fa-user-circle fa-4x p-2" aria-hidden="true"></i>
     </div>

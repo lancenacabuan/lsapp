3f9009976c14054393f1f4129910ff62926404fc
c4lance <lancenacabuan@outlook.com>
2022-03-10 12:34:33 +0800

    change password

--- resources/views/pages/changepassword.blade.php
+++ resources/views/pages/changepassword.blade.php
@@ -1,37 +1,32 @@
 @extends('layouts.app')
 @section('content')
-<center>
 <div class="container-fluid">
-<div class="card" style="width: 700px;">   
-    <div class="card-body" style="background-color:white;color:black;">
-    <h2>Change Password</h2>
-    <hr>
-        <input type="hidden" name="_token" id="csrf" value="{{Session::token()}}">
-        <div class="input-group mb-2">
-            <div class="input-group-prepend">
-                <label class="input-group-text" style="width: 150px;">Current Password</label>
+    <div class="card mx-auto" style="width: 500px;">   
+        <div class="card-body" style="background-color: white; color: black; text-align: center;">
+        <h2>Change Password</h2>
+        <hr>
+            <input type="hidden" name="_token" id="csrf" value="{{Session::token()}}">
+            <div class="input-group mb-2">
+                <div class="input-group-prepend">
+                    <label class="input-group-text" style="width: 150px;">Current Password</label>
+                </div>
+                <input type="password" id="pass1" style="width: 316px;" autofocus>
             </div>
-            <input type="password" id="pass1" style="width: 500px;" autofocus>
-        </div>
-        <div class="input-group mb-2">
-            <div class="input-group-prepend">
-                <label class="input-group-text" style="width: 150px;">New Password</label>
+            <div class="input-group mb-2">
+                <div class="input-group-prepend">
+                    <label class="input-group-text" style="width: 150px;">New Password</label>
+                </div>
+                <input type="password" id="pass2" style="width: 316px;">
             </div>
-            <input type="password" id="pass2" style="width: 500px;">
-        </div>
-        <div class="input-group mb-2">
-            <div class="input-group-prepend">
-                <label class="input-group-text" style="width: 150px;">Confirm Password</label>
+            <div class="input-group mb-2">
+                <div class="input-group-prepend">
+                    <label class="input-group-text" style="width: 150px;">Confirm Password</label>
+                </div>
+                <input type="password" id="pass3" style="width: 316px;">
             </div>
-            <input type="password" id="pass3" style="width: 500px;">
+            <br/>
+            <button type="submit" id="savepassword" class="btn btn-primary float-right bp">UPDATE</button>
         </div>
-        <br/>
-        <center>
-            <button type="submit" id="savepassword" class="btn btn-primary float-right bp" style="margin-right: 18px;">
-            UPDATE</button>
-        </center>
     </div>
 </div>
-</div>
-<center>
 @endsection

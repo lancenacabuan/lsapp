0b139c3fd0d65bb7528b4b3996fae14d922d0058
c4lance <lancenacabuan@outlook.com>
2022-03-11 17:27:04 +0800

    guest

--- resources/views/inc/guest.blade.php
+++ resources/views/inc/guest.blade.php
@@ -2,4 +2,3 @@
     <div style="text-align: center;"><img class="p-2" src="/storage/idsi.png" alt="idsi.png" style="width: auto; height: 150px;"></div>
     <div style="text-align: center; height: 90px; line-height: 90px; color: white; font-family: Arial; background-color: #0d1a80; font-size: 30px; font-weight: bold;">MAIN WAREHOUSE STOCK MONITORING SYSTEM</div>
 </a>
-<br>

8941a09e74c45ae18583bc03a36bbe98922653ba
c4lance <lancenacabuan@outlook.com>
2022-05-17 16:13:12 +0800

    v1.0.2.3

--- resources/views/inc/header.blade.php
+++ resources/views/inc/header.blade.php
@@ -1,4 +1,4 @@
-<div class="d-flex xD" title="v1.0.2.2">
+<div class="d-flex xD mb-2" title="v1.0.2.3">
     <a href="/">
         <img class="p-2 align-self-end" src="{{asset('idsi.png')}}" style="width: auto; height: 90px; line-height: 90px;">
     </a>

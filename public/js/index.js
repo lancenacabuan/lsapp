abcf8c75e1043400094a8041bcf0f2cc357c99ae
lancenacabua <lancenacabuan@outlook.com>
2022-06-16 09:18:44 +0000

    changed index icons

--- public/js/index.js
+++ public/js/index.js
@@ -48,11 +48,13 @@ setInterval(function(){
 $(document).ready(function(){
     $('#hover1').hover(
         function(){
+            $('.zoom1').attr('src', 'index-stocks-hover.png').show();
             $('.zoom1').css({"zoom": "110%"});
             $('.box1').css({"zoom": "82%", "background-color": "#0d6efd"});
             $('.text1').css({"color": "#0d6efd"});
         },
         function(){
+            $('.zoom1').attr('src', 'index-stocks.png').show();
             $('.zoom1').css({"zoom": "100%"});
             $('.box1').css({"zoom": "100%", "background-color": "#0d1a80"});
             $('.text1').css({"color": "#0d1a80"});
@@ -63,11 +65,13 @@ $(document).ready(function(){
 $(document).ready(function(){
     $('#hover2').hover(
         function(){
+            $('.zoom2').attr('src', 'index-stockrequest-hover.png').show();
             $('.zoom2').css({"zoom": "110%"});
             $('.box2').css({"zoom": "82%", "background-color": "#0d6efd"});
             $('.text2').css({"color": "#0d6efd"});
         },
         function(){
+            $('.zoom2').attr('src', 'index-stockrequest.png').show();
             $('.zoom2').css({"zoom": "100%"});
             $('.box2').css({"zoom": "100%", "background-color": "#0d1a80"});
             $('.text2').css({"color": "#0d1a80"});
@@ -78,11 +82,13 @@ $(document).ready(function(){
 $(document).ready(function(){
     $('#hover3').hover(
         function(){
+            $('.zoom3').attr('src', 'index-stocktransfer-hover.png').show();
             $('.zoom3').css({"zoom": "110%"});
             $('.box3').css({"zoom": "82%", "background-color": "#0d6efd"});
             $('.text3').css({"color": "#0d6efd"});
         },
         function(){
+            $('.zoom3').attr('src', 'index-stocktransfer.png').show();
             $('.zoom3').css({"zoom": "100%"});
             $('.box3').css({"zoom": "100%", "background-color": "#0d1a80"});
             $('.text3').css({"color": "#0d1a80"});
@@ -93,11 +99,13 @@ $(document).ready(function(){
 $(document).ready(function(){
     $('#hover4').hover(
         function(){
+            $('.zoom4').attr('src', 'index-defective-hover.png').show();
             $('.zoom4').css({"zoom": "110%"});
             $('.box4').css({"zoom": "82%", "background-color": "#0d6efd"});
             $('.text4').css({"color": "#0d6efd"});
         },
         function(){
+            $('.zoom4').attr('src', 'index-defective.png').show();
             $('.zoom4').css({"zoom": "100%"});
             $('.box4').css({"zoom": "100%", "background-color": "#0d1a80"});
             $('.text4').css({"color": "#0d1a80"});

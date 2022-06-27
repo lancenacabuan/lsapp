ce06859e8301078e750996f1894bbb9af501e693
lancenacabua <lancenacabuan@outlook.com>
2022-06-27 07:17:27 +0000

    v1.0.4.7 patch

--- resources/views/pages/stocks.blade.php
+++ resources/views/pages/stocks.blade.php
@@ -120,7 +120,7 @@
             <input type="hidden" name="y_serial" id="y_serial">
             <input style="margin-bottom: 8px; font-size: 12px;" class="form-control form-control-sm" type="text" name="x_category" id="x_category" readonly>
             <textarea style="margin-bottom: 8px; font-size: 12px; resize: none;" class="form-control" rows="5" name="x_item" id="x_item" readonly></textarea>
-            <input style="margin-bottom: 8px; font-size: 12px;" class="form-control form-control-sm" type="text" name="x_serial" id="x_serial" placeholder="Input Item Serial." autocomplete="off">
+            <input style="margin-bottom: 8px; font-size: 12px;" class="form-control form-control-sm" type="text" name="x_serial" id="x_serial" placeholder="Input Item Serial" autocomplete="off">
             <br>
             <button type="button" id="btnEdit" class="btn btn-primary float-right bp" style="zoom: 80%;">EDIT</button>
             <span class="float-right" style="width: 5px;">&nbsp;</span>

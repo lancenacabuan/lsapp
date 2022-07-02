d800adb4a6f85121cf52c244115eaae8f9a67910
lancenacabua <lancenacabuan@outlook.com>
2022-07-02 06:36:20 +0000

    1.0.4.8

--- resources/views/inc/header.blade.php
+++ resources/views/inc/header.blade.php
@@ -1,4 +1,4 @@
-<div class="d-flex xD" title="v1.0.4.7" style="height: 90px;">
+<div class="d-flex xD" title="v1.0.4.8" style="height: 90px;">
     <a href="/">
         <img class="p-2 align-self-end" src="{{asset('idsi.png')}}" style="width: auto; height: 90px; line-height: 90px;">
     </a>

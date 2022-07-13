b0b8df7bcf711833ef9f113a3d8d8b363e8fb3f2
lancenacabua <lancenacabuan@outlook.com>
2022-07-13 07:29:14 +0000

    fix received requests visibility

--- app/Http/Controllers/StockRequestController.php
+++ app/Http/Controllers/StockRequestController.php
@@ -539,7 +539,8 @@ class StockRequestController extends Controller
             $list = Requests::selectRaw('DATE_FORMAT(requests.created_at, "%b. %d, %Y") AS reqdatetime, DATE_FORMAT(requests.needdate, "%b. %d, %Y") AS needdatetime, DATE_FORMAT(requests.created_at, "%Y-%m-%d") AS reqdate, requests.id AS req_id, requests.created_at AS date, requests.request_number AS req_num, requests.requested_by AS user_id, request_type.name AS req_type, status.status AS status, users.name AS req_by, request_type.id AS req_type_id, status.id AS status_id, requests.schedule AS sched, prepared_by, client_name, location, contact, remarks, reference, reason, needdate, requests.item_id AS item_id, qty, assembly_reqnum, reference_upload, orderID, asset_reqby, asset_apvby, asset_reqby_email, asset_apvby_email, notify')
                 ->where('users.company', auth()->user()->company)
                 ->whereIn('request_type.id', ['2','3'])
-                ->whereNotIn('requests.status', ['7','8','10','26','29'])
+                ->whereNotIn('requests.notify', ['confirmed'])
+                ->whereNotIn('requests.status', ['7','26','29'])
                 ->join('users', 'users.id', '=', 'requests.requested_by')
                 ->join('request_type', 'request_type.id', '=', 'requests.request_type')
                 ->join('status', 'status.id', '=', 'requests.status')
@@ -550,7 +551,8 @@ class StockRequestController extends Controller
         else if(auth()->user()->hasanyRole('accounting')){ //---ROLES---//
             $list = Requests::selectRaw('DATE_FORMAT(requests.created_at, "%b. %d, %Y") AS reqdatetime, DATE_FORMAT(requests.needdate, "%b. %d, %Y") AS needdatetime, DATE_FORMAT(requests.created_at, "%Y-%m-%d") AS reqdate, requests.id AS req_id, requests.created_at AS date, requests.request_number AS req_num, requests.requested_by AS user_id, request_type.name AS req_type, status.status AS status, users.name AS req_by, request_type.id AS req_type_id, status.id AS status_id, requests.schedule AS sched, prepared_by, client_name, location, contact, remarks, reference, reason, needdate, requests.item_id AS item_id, qty, assembly_reqnum, reference_upload, orderID, asset_reqby, asset_apvby, asset_reqby_email, asset_apvby_email, notify')
                 ->whereIn('request_type.id', ['2','3','6'])
-                ->whereNotIn('requests.status', ['7','8','10','26','29'])
+                ->whereNotIn('requests.notify', ['confirmed'])
+                ->whereNotIn('requests.status', ['7','26','29'])
                 ->join('users', 'users.id', '=', 'requests.requested_by')
                 ->join('request_type', 'request_type.id', '=', 'requests.request_type')
                 ->join('status', 'status.id', '=', 'requests.status')
@@ -560,7 +562,8 @@ class StockRequestController extends Controller
         }
         else if(auth()->user()->hasanyRole('admin') || auth()->user()->hasanyRole('encoder') || auth()->user()->hasanyRole('viewer')){ //---ROLES---//
             $list = Requests::selectRaw('DATE_FORMAT(requests.created_at, "%b. %d, %Y") AS reqdatetime, DATE_FORMAT(requests.needdate, "%b. %d, %Y") AS needdatetime, DATE_FORMAT(requests.created_at, "%Y-%m-%d") AS reqdate, requests.id AS req_id, requests.created_at AS date, requests.request_number AS req_num, requests.requested_by AS user_id, request_type.name AS req_type, status.status AS status, users.name AS req_by, request_type.id AS req_type_id, status.id AS status_id, requests.schedule AS sched, prepared_by, client_name, location, contact, remarks, reference, reason, needdate, requests.item_id AS item_id, qty, assembly_reqnum, reference_upload, orderID, asset_reqby, asset_apvby, asset_reqby_email, asset_apvby_email, notify')
-                ->whereNotIn('requests.status', ['7','8','10','14','19','26','29'])
+                ->whereNotIn('requests.notify', ['confirmed'])
+                ->whereNotIn('requests.status', ['7','14','19','26','29'])
                 ->join('users', 'users.id', '=', 'requests.requested_by')
                 ->join('request_type', 'request_type.id', '=', 'requests.request_type')
                 ->join('status', 'status.id', '=', 'requests.status')
@@ -571,7 +574,8 @@ class StockRequestController extends Controller
         else{
             $list = Requests::selectRaw('DATE_FORMAT(requests.created_at, "%b. %d, %Y") AS reqdatetime, DATE_FORMAT(requests.needdate, "%b. %d, %Y") AS needdatetime, DATE_FORMAT(requests.created_at, "%Y-%m-%d") AS reqdate, requests.id AS req_id, requests.created_at AS date, requests.request_number AS req_num, requests.requested_by AS user_id, request_type.name AS req_type, status.status AS status, users.name AS req_by, request_type.id AS req_type_id, status.id AS status_id, requests.schedule AS sched, prepared_by, client_name, location, contact, remarks, reference, reason, needdate, requests.item_id AS item_id, qty, assembly_reqnum, reference_upload, orderID, asset_reqby, asset_apvby, asset_reqby_email, asset_apvby_email, notify')
                 ->where('requests.requested_by', auth()->user()->id)
-                ->whereNotIn('requests.status', ['8','10','14','19','26','29'])
+                ->whereNotIn('requests.notify', ['confirmed'])
+                ->whereNotIn('requests.status', ['14','19','26','29'])
                 ->join('users', 'users.id', '=', 'requests.requested_by')
                 ->join('request_type', 'request_type.id', '=', 'requests.request_type')
                 ->join('status', 'status.id', '=', 'requests.status')
@@ -1928,6 +1932,10 @@ class StockRequestController extends Controller
                             ->update(['status' => '8']);
                     }
                     while(!$sql);
+                    if($request->request_type == '1' || $request->request_type == '4' || $request->request_type == '5' || $request->request_type == '6'){
+                        Requests::where('request_number', $request->request_number)
+                            ->update(['notify' => 'confirmed']);
+                    }
                 }
                 else{
                     do{

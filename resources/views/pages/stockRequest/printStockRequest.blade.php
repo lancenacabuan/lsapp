0996a8b46306b0dd9d786d8f556230e244c6e401
lancenacabua <lancenacabuan@outlook.com>
2022-07-19 05:39:48 +0000

    new feature for staging#3 DR

--- resources/views/pages/stockRequest/printStockRequest.blade.php
+++ resources/views/pages/stockRequest/printStockRequest.blade.php
@@ -57,7 +57,7 @@
             <tr height="20">
                 <td colspan="9">&nbsp;</td>
             </tr>
-            @if($list->req_type_id == 2 || ($list->req_type_id == 3 && ($list->status_id == 10 || $list->status_id >= 27)))
+            @if($list->req_type_id == 2 || $list->req_type_id == 8 || ($list->req_type_id == 3 && ($list->status_id == 10 || $list->status_id >= 27)))
             <tr height="20">
                 <td colspan="1" style="font-weight: bold; width: 200px;">Delivery Receipt No.:</td>
                 <td colspan="8">{{$list->req_num}}</td>
@@ -176,7 +176,7 @@
                 <td colspan="2" class="tdHide">{{$list->remarks}}</td>
             </tr>
             <tr height="20">
-                @if($list->req_type_id == 2 || ($list->req_type_id == 3 && ($list->status_id == 10 || $list->status_id >= 27)))
+                @if($list->req_type_id == 2 || $list->req_type_id == 8 || ($list->req_type_id == 3 && ($list->status_id == 10 || $list->status_id >= 27)))
                 <td colspan="2" style="font-weight: bold;" class="tdHide">Reference SO/PO No.:</td>
                 <td colspan="7" class="tdHide">{{$list->reference}}</td>
                 @else
